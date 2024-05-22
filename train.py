793c6c37a83cdf6f742019a30af3c6c7b4edf6a8
zhuxiaoming <952801349@qq.com>
2024-05-22 10:51:59 +0800

    修复bug，能work了

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ bad_label_list = ["porn", "hentai"]
 # testfile  https://github.com/LAION-AI/CLIP-based-NSFW-Detector/blob/main/nsfw_testset.zip 据说标注过
 
 class ModelTrainer:
-    def __init__(self, data_dir: str, model_dir: str, dim=768,
+    def __init__(self, data_dir: str, model_dir: str, dim=768, device="cuda",
                  additional_train_label_emb_dict=None,
                  additional_test_label_emb_dict=None):
         # data
@@ -25,10 +25,13 @@ class ModelTrainer:
         self.additional_test_label_emb_dict = additional_test_label_emb_dict
         self.data_dir = data_dir
         os.makedirs(data_dir, exist_ok=True)
-        self.model = H14_NSFW_Detector(dim, len(label_ind_dict))
+        self.model = H14_NSFW_Detector(dim, len(label_ind_dict)).to(device)
         self.model_dir = model_dir
+        self.device = device
+        os.makedirs(self.model_dir, exist_ok=True)
 
     def download_origin_data(self, ):
+        # 国内网络问题，未debug，可直接下载后放入文件夹
         train_url = "https://drive.google.com/file/d/1yenil0R4GqmTOFQ_GVw__x61ofZ-OBcS/view?usp=sharing"
         res = os.system(
             f"wget --no-check-certificate '{train_url}' -O {self.data_dir}/train.zip")
@@ -37,8 +40,8 @@ class ModelTrainer:
         res = os.system(
             f"wget --no-check-certificate '{test_url}' -O {self.data_dir}/test.zip")
         assert res == 0
-        os.system(f"unzip {self.data_dir}/train.zip -d {self.data_dir}")
-        os.system(f"unzip {self.data_dir}/test.zip -d {self.data_dir}")
+        os.system(f"unzip {self.data_dir}/train.zip -d {self.data_dir}/train")
+        os.system(f"unzip {self.data_dir}/test.zip -d {self.data_dir}/test")
 
     def load_dataloader(self):
         train_emb_dict = self.load_dir_emb_dict(os.path.join(self.data_dir, "train"))
@@ -59,7 +62,7 @@ class ModelTrainer:
         train_dataset = CustomDataset(train_data_list, label_ind_dict)
         val_dataset = CustomDataset(val_data_list, label_ind_dict)
         train_dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
-        val_dataloader = DataLoader(val_dataset, batch_size=128, shuffle=False)
+        val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=False)
         test_data_list = []
         for k, v in test_emb_dict.items():
             label = k.split("-")[0]
@@ -81,55 +84,33 @@ class ModelTrainer:
             if os.path.exists(emd_file_path):
                 dir_emb_dict[dir1] = np.load(emd_file_path)
         return dir_emb_dict
-
     def train(self, epoch_amount):
+        # 主函数，需要先下载数据之后就可以了
+        train_dataloader, val_dataloader, test_dataloader = self.load_dataloader()
         optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
         loss_func = torch.nn.CrossEntropyLoss()
-        save_dir = "./train_model"
-        os.makedirs(save_dir, exist_ok=True)
-        val_acc_list = []
-        test_acc_list = []
         for epoch in range(epoch_amount):
             self.model.train()
-            for i, (data, label) in enumerate(self.train_dataloader):
+            for i, (data, label) in enumerate(train_dataloader):
+                label = label.to(self.device)
                 optimizer.zero_grad()
-                output = self.model(data)
+                output = self.model(data.to(self.device))
                 loss = loss_func(output, label)
                 loss.backward()
                 optimizer.step()
-                if i % 10 == 0:
+                if i % 100 == 0:
                     print(f"epoch: {epoch}, step: {i}, loss: {loss.item()}")
-            self.model.eval()
-            label_count_dict = defaultdict(int)
-            label_acc_dict = defaultdict(int)
-            with torch.no_grad():
-                correct = 0
-                total = 0
-                for i, (data, label) in enumerate(self.val_dataloader):
-                    output = self.model(data)
-                    _, predicted = torch.max(output, 1)
-                    total += label.size(0)
-                    correct += (predicted == label).sum().item()
-                    for p, l in zip(predicted, label):
-                        if p == l:
-                            label_acc_dict[int(l)] += 1
-                        label_count_dict[int(l)] += 1
+            print("val")
+            self.show_model_dataloader_result(self.model, val_dataloader)
+            print("test")
+            self.show_model_dataloader_result(self.model, test_dataloader)
 
-                log(f"epoch: {epoch}, accuracy: {correct / total}")
-                val_acc_list.append(correct / total)
-            # with torch.no_grad():
-            #     correct = 0
-            #     total = 0
-            #     for i, (data, label) in enumerate(self.test_dataloader):
-            #         output = self.model(data)
-            #         _, predicted = torch.max(output, 1)
-            #         total += label.size(0)
-            #         correct += (predicted == label).sum().item()
-            #     log(f"epoch: {epoch}, accuracy: {correct / total}")
-            #     test_acc_list.append(correct / total)
-            torch.save(self.model, os.path.join(save_dir, f"model_{epoch}.pth"))
+            torch.save(self.model.state_dict(), os.path.join(self.model_dir, f"model_{epoch}.pth"))
 
-    def show_model_dataloader_result(self, model, dataloader):
+    @staticmethod
+    def show_model_dataloader_result(model, dataloader):
+        # 测试集drawing效果较差，不确定是不是测试集数据or训练数据集问题，但是本身不影响nsfw的训练，
+        # 本身有展示bad 和good的ratio
         model.eval()
         label_count_dict = defaultdict(int)
         label_acc_dict = defaultdict(int)
@@ -138,13 +119,15 @@ class ModelTrainer:
             correct = 0
             total = 0
             for i, (data, label) in enumerate(dataloader):
-                output = model(data)
+                output = model(data.to(self.device)).cpu()
                 _, predicted = torch.max(output, 1)
                 total += label.size(0)
                 correct += (predicted == label).sum().item()
                 for p, l in zip(predicted, label):
                     if p == l:
                         label_acc_dict[int(l)] += 1
+                    else:
+                        label_error_count_dict[int(l)][int(p)] += 1
                     label_count_dict[int(l)] += 1
 
         print(f"accuracy: {correct / total}")
@@ -173,11 +156,13 @@ class ModelTrainer:
 
             result.append(
                 dict(label=label, amount=amount, acc=acc, aac_ratio=acc_ratio,
-                     error_count_dict=new_error_count_dict, good_ratio=round(good_amount / amount * 100, 1),
-                     bad_ratio=round(bad_amount / amount * 100, 1))
+                     good_ratio=round(good_amount / amount * 100, 1),
+                     bad_ratio=round(bad_amount / amount * 100, 1), error_count_dict=new_error_count_dict, )
             )
 
         print(pd.DataFrame(result))
+        return result
 
 
-self = ModelTrainer("./train", "./test/nsfw_testset/", 768)
+
+self = ModelTrainer("./nsfw_data", "./tmp+model", 768)
